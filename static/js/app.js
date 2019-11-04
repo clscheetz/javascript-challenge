c409a5e0294c8b9e9330ef135fa6327d6490d416
Chelsea Scheetz <clscheetz@gmail.com>
2019-11-03 22:16:14 -0500

    finished level 2

--- static/js/app.js
+++ static/js/app.js
@@ -25,13 +25,39 @@ const button = d3.select("#filter-btn");
 button.on("click", function() {
 
     // Select input element
-    let inputElement = d3.select("#datetime");
+    let inputElement = d3.select("#searchkey");
+
+    // Select the selected element
+    let searchType = d3.select("#searchtype").property("value");
 
     // Get value prop. of input element
     let inputValue = inputElement.property("value");
 
-    console.log(inputValue);
-    console.log(tableData);
+    // Return total data set
+    console.log('input: ' + inputValue);
+    // Return filtered data set
+    console.log('full dataset size: ' + tableData.length);
 
-    // let filteredData = tableData.filter(sighting => sighting.datetime == inputValue);
+    // Filter data based on date/time of sighting
+    let filteredData = tableData.filter(sighting => sighting[searchType] === inputValue);
+    console.log('filtered dataset size: ' + filteredData.length);
+    
+    // Remove any sightings from the tbody
+    tbody.html("");
+
+    // Repopulate table rows
+    filteredData.forEach((sighting) => {
+        console.log(sighting);
+    
+        // Add 'tr' for each sighting
+        const tr = tbody.append("tr");
+    
+        // Use Object.entries to console for each sighting
+        Object.entries(sighting).forEach(([key, value]) => {
+            console.log(`${key}: ${value}`);
+    
+            // Add values to each table cell
+            tr.append("td").text(value);
+        });
     });
+});
